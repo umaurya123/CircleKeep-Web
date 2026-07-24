af8a2bf68ae58a0123ff271a95b42ff4ede4cf5b
maurya-s <sumanmaurya@yahoo.com>
2026-07-24 19:20:35 -0400

    Bump versionCode to 3 and versionName to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,8 +93,8 @@ android {
         applicationId = "com.circlekeep"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
