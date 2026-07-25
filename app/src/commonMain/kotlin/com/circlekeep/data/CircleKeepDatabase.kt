9d8f2c2172dadc557ccca779234a58dfaa67662d
maurya-s <sumanmaurya@yahoo.com>
2026-07-25 14:27:50 -0400

    Fix Room KMP error: Add @ConstructedBy and AppDatabaseConstructor to CircleKeepDatabase

--- app/src/commonMain/kotlin/com/circlekeep/data/CircleKeepDatabase.kt
+++ app/src/commonMain/kotlin/com/circlekeep/data/CircleKeepDatabase.kt
@@ -1,13 +1,22 @@
 package com.circlekeep.data
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
 import androidx.room.TypeConverters
 
 @Database(entities = [Friend::class, Child::class, Group::class], version = 15, exportSchema = false)
 @TypeConverters(Converters::class)
+@ConstructedBy(AppDatabaseConstructor::class)
 abstract class CircleKeepDatabase : RoomDatabase() {
     abstract fun friendDao(): FriendDao
+}
+
+// The Room compiler generates the implementation of this class
+@Suppress("NO_ACTUAL_FOR_EXPECT")
+expect object AppDatabaseConstructor : RoomDatabaseConstructor<CircleKeepDatabase> {
+    override fun initialize(): CircleKeepDatabase
 }
 
 fun getRoomDatabase(
