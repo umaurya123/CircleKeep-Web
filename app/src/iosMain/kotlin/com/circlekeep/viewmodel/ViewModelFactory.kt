5a0615ae1e852fe7072c2689878e9d88879cd190
maurya-s <sumanmaurya@yahoo.com>
2026-07-25 14:43:20 -0400

    Add iOS actual implementation for FriendViewModelFactory and its dependencies

---
+++ app/src/iosMain/kotlin/com/circlekeep/viewmodel/ViewModelFactory.kt
@@ -0,0 +1,48 @@
+package com.circlekeep.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.circlekeep.data.CircleKeepDatabase
+import com.circlekeep.data.FriendRepository
+import com.circlekeep.data.UserPreferencesRepository
+import com.circlekeep.data.getDatabaseBuilder
+import com.circlekeep.data.getRoomDatabase
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import okio.Path.Companion.toPath
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSURL
+import platform.Foundation.NSUserDomainMask
+import kotlinx.cinterop.ExperimentalForeignApi
+
+private val database: CircleKeepDatabase by lazy {
+    getRoomDatabase(getDatabaseBuilder())
+}
+
+private val repository: FriendRepository by lazy {
+    FriendRepository(database.friendDao())
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private val userPreferencesRepository: UserPreferencesRepository by lazy {
+    val documentDirectory: NSURL = NSFileManager.defaultManager.URLForDirectory(
+        directory = NSDocumentDirectory,
+        inDomain = NSUserDomainMask,
+        appropriateForURL = null,
+        create = false,
+        error = null
+    )!!
+    val path = (documentDirectory.path!! + "/circlekeep.preferences_pb")
+    UserPreferencesRepository(
+        PreferenceDataStoreFactory.create(
+            produceFile = { path }
+        )
+    )
+}
+
+actual fun getFriendViewModelFactory(): ViewModelProvider.Factory = viewModelFactory {
+    initializer {
+        FriendViewModel(repository, userPreferencesRepository)
+    }
+}
